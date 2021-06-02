57f266f251d55078b52ef6a49fed5f7dafa1db4e
liranmauda <liran.mauda@gmail.com>
2021-06-02 17:24:42 +0300

    typo

--- src/util/background_scheduler.js
+++ src/util/background_scheduler.js
@@ -31,7 +31,7 @@ class Background_Scheduler {
     // so that it won't prevent the process from existing if it's the only timer left
     run_background_worker(worker) {
         const self = this;
-        const DEFUALT_DELAY = 10000;
+        const DEFAULT_DELAY = 10000;
         this.workers_by_name_cache[worker.name] = worker;
 
         function run() {
@@ -40,10 +40,10 @@ class Background_Scheduler {
                         return worker.run_batch();
                     })
                     .then(function(delay) {
-                        return P.delay_unblocking(delay || worker.delay || DEFUALT_DELAY);
+                        return P.delay_unblocking(delay || worker.delay || DEFAULT_DELAY);
                     }, function(err) {
                         dbg.log('run_background_worker', worker.name, 'UNCAUGHT ERROR', err, err.stack);
-                        return P.delay_unblocking(worker.delay || DEFUALT_DELAY);
+                        return P.delay_unblocking(worker.delay || DEFAULT_DELAY);
                     })
                     .then(run);
             }
@@ -51,7 +51,7 @@ class Background_Scheduler {
         dbg.log('run_background_worker:', 'INIT', worker.name);
         let initial_delay = 0;
         if (!worker.run_immediate) {
-            initial_delay = worker.boot_delay || worker.delay || DEFUALT_DELAY;
+            initial_delay = worker.boot_delay || worker.delay || DEFAULT_DELAY;
         }
         P.delay_unblocking(initial_delay).then(run);
         return worker;
